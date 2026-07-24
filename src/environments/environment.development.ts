de5b881ef4ab5db6c2edbd85ed39fe6d618f8d9b
punjabcanalchd <punjabcanalchd@gmail.com>
2026-07-24 12:01:27 +0530

    Url updated
    Url updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
 
     production: false,
     appName: 'PWRDA',
-    apiUrl: 'http://127.0.0.1:8000/api',
+    apiUrl: 'http://localhost:8000/api',
     appVersion: '1.0.0',
     requestTimeout: 30000,
     enableLogs: true,
